c219791e41f72b2cefc4a06b9fb7b2c1f818766f
csillagkrisztian <krisztian.csillag93@gmail.com>
2020-08-10 15:23:46 +0200

    added code editor from a previous project, HomePage, PlaygroundPage and installed all dependencies

---
+++ src/pages/Playground/PlaygroundPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CodePlayground from "../../components/CodePlayground/CodePlayground";
+
+export default function Playground() {
+  return (
+    <div>
+      <h1>Welcome to the Playground</h1>
+      <CodePlayground />
+    </div>
+  );
+}
