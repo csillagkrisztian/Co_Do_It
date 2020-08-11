bd8add773890ca2c05991e715e44f1f4b391f222
csillagkrisztian <krisztian.csillag93@gmail.com>
2020-08-11 14:59:12 +0200

    Set up the code editor business logic for the puzzles

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -14,7 +14,7 @@ export default function Navigation() {
   const loginLogoutControls = token ? <LoggedIn /> : <LoggedOut />;
 
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar className="color-nav" bg="dark" variant="dark" expand="lg">
       <Navbar.Brand as={NavLink} to="/">
         Co_Do_It
       </Navbar.Brand>
