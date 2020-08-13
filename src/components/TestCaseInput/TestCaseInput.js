eaf8e824be4072f67542e5eb307ad97b88ea70d1
csillagkrisztian <krisztian.csillag93@gmail.com>
2020-08-13 22:34:48 +0200

    added error checking logic to the test cases

--- src/components/TestCaseInput/TestCaseInput.js
+++ src/components/TestCaseInput/TestCaseInput.js
@@ -4,6 +4,9 @@ import { Form, Button } from "react-bootstrap";
 export default function TestCaseInput(props) {
   const [given, setGiven] = useState("");
   const [result, setResult] = useState("");
+  const [givenError, setGivenError] = useState(false);
+  const [resultError, setResultError] = useState(false);
+  const [ready, setReady] = useState(false);
   return (
     <>
       <br></br>
@@ -26,14 +29,58 @@ export default function TestCaseInput(props) {
         placeholder="Enter a result"
         required
       />
-      <Button
-        className="mt-3 mb-2"
-        onClick={() => {
-          console.log({ given, result });
-        }}
-      >
-        Check Test Case
-      </Button>
+      {!ready ? (
+        <Button
+          disabled={!given || !result}
+          className="mt-3 mb-2"
+          onClick={() => {
+            try {
+              const stuff = given.split("\n");
+              console.log("stuff", stuff);
+              const check = stuff.filter((things) => {
+                return things[things.length - 1] === ";";
+              });
+              console.log("check", check);
+              console.log();
+              if (stuff.length > check.length) {
+                setGivenError(true);
+              } else {
+                eval(`
+                const console = null;
+                const document = null;
+                const location = null;
+                const window = null;
+                ${given}`);
+                setGivenError(false);
+              }
+            } catch (error) {
+              setGivenError(true);
+            }
+            try {
+              const hey = JSON.parse(`${result}`);
+              if (Array.isArray(hey)) {
+                console.log("it's an array");
+                setResultError(false);
+              }
+              console.log({ given, result });
+            } catch (error) {
+              setResultError(true);
+            }
+
+            setReady(true);
+          }}
+        >
+          Check Test Case
+        </Button>
+      ) : (
+        <Form.Text className="text">Ready!</Form.Text>
+      )}
+      <br></br>
+      {givenError &&
+        "Are you sure those are valid variables? It needs a ';' at the end!"}
+      <br></br>
+      {resultError &&
+        "Are you sure that is a valid result? It needs to be an array"}
     </>
   );
 }
