6a1780765f0202af35f685aeace8a543ccd5ac6a
csillagkrisztian <krisztian.csillag93@gmail.com>
2020-08-19 17:35:32 +0200

    added the users profile picture to the navbar

--- src/components/Navigation/LoggedIn.js
+++ src/components/Navigation/LoggedIn.js
@@ -5,6 +5,7 @@ import Button from "react-bootstrap/Button";
 import { selectUser } from "../../store/user/selectors";
 import Nav from "react-bootstrap/Nav";
 import NavbarItem from "./NavbarItem";
+import { NavbarBrand } from "react-bootstrap";
 
 export default function LoggedIn() {
   const dispatch = useDispatch();
@@ -19,9 +20,16 @@ export default function LoggedIn() {
       )}
       <NavbarItem path={"/battle/"} linkText="Battle"></NavbarItem>
       <NavbarItem path={"/myprofile"} linkText="My Profile"></NavbarItem>
+
       <Nav.Item style={{ color: "white", padding: ".5rem 1rem" }}>
         {user.name}
       </Nav.Item>
+      <NavbarBrand>
+        <img
+          src={user.imageUrl}
+          style={{ width: "30px", height: "30px", marginLeft: "15px" }}
+        />
+      </NavbarBrand>
       <Button onClick={() => dispatch(logOut())}>Logout</Button>
     </>
   );
